a10d00e8002f522a201de4a714be98e61f290101
Satoshi Morita <shiroemons@gmail.com>
2022-01-18 19:19:01 +0900

    :cop: rubocop --auto-correct --only Style/ColonMethodCall

--- test/cuid_test.rb
+++ test/cuid_test.rb
@@ -4,36 +4,36 @@ require 'test/unit'
 
 class CuidTest < Test::Unit::TestCase
   def test_generate_string
-    c = Cuid::generate
+    c = Cuid.generate
     assert c.is_a? String
   end
 
   def test_generate_array
-    c = Cuid::generate(3)
+    c = Cuid.generate(3)
     assert c.is_a? Array
     assert_equal 3, c.size
   end
 
   def test_generate_format
-    c = Cuid::generate
+    c = Cuid.generate
     assert_equal 25, c.length
     assert c.start_with? "c"
   end
 
   def test_validate_true
-    c = Cuid::generate
-    assert Cuid::validate(c)
+    c = Cuid.generate
+    assert Cuid.validate(c)
   end
 
   def test_validate_false
     c = "d00000000000000000000"
-    assert !Cuid::validate(c)
+    assert !Cuid.validate(c)
   end
 
   def test_collision
     results = {}
     collision = false
-    c = Cuid::generate(600000)
+    c = Cuid.generate(600000)
     c.each do |e|
       collision = true if results[e]
       results[e] = true
@@ -44,7 +44,7 @@ class CuidTest < Test::Unit::TestCase
   def test_secure_random
     results = {}
     collision = false
-    c = Cuid::generate(1000, true)
+    c = Cuid.generate(1000, true)
     c.each do |e|
       collision = true if results[e]
       results[e] = true
